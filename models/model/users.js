60f0a8ec6665473c0460abe2af21e39812796ffb
Pradeep <pksingh0315@gmail.com>
2023-03-14 11:25:01 +0530

    models

--- models/model/users.js
+++ models/model/users.js
@@ -1,11 +1,14 @@
 const mongoose=require('mongoose');
-
+//schema
 const userSchema=new mongoose.Schema({
     name:{type:String, require:true},
     age:{type:Number, min:18, max:60},
     email:{type:String, lowercase:true, trim:true},
     password:{type:String, minlength:5, maxlenght:10}
+
 })
-const Tank=mongoose.Model("Tank",userSchema)
-// for export
+
+// model
+const Tank=mongoose.model("Tank",userSchema)
+// for model export
 module.exports=Tank;
